65be91bf8ec7b6c98b12ed228c4b1fc94b20fc3a
f1nalspace <finalspace@googlemail.com>
2023-06-27 18:19:01 +0200

    Added haversine lib that contains types, math and rdtsc

---
+++ HaversineGenerator/HaversineLib/HaversineMath.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Final.PerformanceAwareCourse
+{
+    public static class HaversineMath
+    {
+        public const double EarthRadius = 6372.8;
+
+        const double Deg2RadFactor = Math.PI / 180.0;
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        static double RadiansFromDegrees(double degrees) => degrees * Deg2RadFactor;
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        static double Square(double value) => value * value;
+
+        public static double HaversineDistance(double x0, double y0, double x1, double y1, double radius = EarthRadius)
+        {
+            double lat1 = y0;
+            double lat2 = y1;
+            double lon1 = x0;
+            double lon2 = x1;
+
+            double dLat = RadiansFromDegrees(lat2 - lat1);
+            double dLon = RadiansFromDegrees(lon2 - lon1);
+            lat1 = RadiansFromDegrees(lat1);
+            lat2 = RadiansFromDegrees(lat2);
+
+            double a = Square(Math.Sin(dLat / 2.0)) + Square(Math.Sin(dLon / 2.0)) * Math.Cos(lat1) * Math.Cos(lat2);
+            double c = 2.0 * Math.Asin(Math.Sqrt(a));
+
+            double result = radius * c;
+
+            return result;
+        }
+    }
+}
---
+++ HaversineGenerator/HaversineLib/HaversinePair.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Final.PerformanceAwareCourse
+{
+    public readonly struct HaversinePair
+    {
+        public double X0 { get; }
+        public double Y0 { get; }
+        public double X1 { get; }
+        public double Y1 { get; }
+
+        public HaversinePair(double x0, double y0, double x1, double y1)
+        {
+            X0 = x0;
+            Y0 = y0;
+            X1 = x1;
+            Y1 = y1;
+        }
+
+        public override string ToString() => FormattableString.Invariant($"{X0:F16}, {Y0:F16}, {X1:F16}, {Y1:F16}");
+    }
+}
